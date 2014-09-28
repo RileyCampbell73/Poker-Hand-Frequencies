3b363961893cf14be8321775296bc1c41e9005c0
Riley Campbell <riley.campbell73@gmail.com>
2014-09-27 17:14:38 -0700

    Some refactoring. Needs lots of work

--- SerialPokerHands/Deck.h
+++ SerialPokerHands/Deck.h
@@ -1,48 +1,60 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <list>
 using namespace std;
 
-class Card{
-private:
-	string value;
-	//string suit;
-public:
-	string suit;
-	Card(){
-		value = "";
-		suit = "";
-	}
-	Card(string val, string st){
-		value = val;
-		suit = st;
-	}
-};
+//class Card{
+//private:
+//	string value;
+//	//string suit;
+//public:
+//	string suit;
+//	Card(){
+//		value = "";
+//		suit = "";
+//	}
+//	Card(string val, string st){
+//		value = val;
+//		suit = st;
+//	}
+//};
 
 class Deck{
 private:
-vector<Card> deck;
+	vector<int> deck;
 public:
-	
+
 	Deck(){
-		string values[13] = { "ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king" };
-		string suits[4] = { "spade", "heart", "diamond", "club" };
+		//builds a deck using ints
+		int values[13] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+		int suits[4] = { 1, 2, 3, 4 };
+		//loop takes both arrays and put all the values with the suits
+		//so 1,1 for example is an ace of spades (1,1)
+		//crude and there definatly a better way to do it. 
+		//but this way will be the easiest in term of sorting and finding matches
 		for (int v = 0; v < 13; v++){
 			for (int s = 0; s < 4; s++){
-				Card temp = Card(values[v], suits[s]);
-				deck.push_back(temp);		
+				string temp = to_string(values[v]) + to_string(suits[s]);
+				deck.push_back(atoi(temp.c_str()));
 			}
 		}
 
 	}
-	vector<Card> getHand(){
-		vector<Card> temp;
-		random_shuffle(deck.begin(), deck.end());
+
+	//Shuffles the deck and then returns 5 'cards' from the top of the deck
+	vector<int> getHand(){
+		vector<int> hand;
+		//shuffle the deck
+		random_shuffle(deck.begin(),deck.end());
+		//populate the hand with 5 cards off the deck
 		for (int i= 0; i < 5; i++){
-			temp.push_back(deck[i]);
+			hand.push_back(deck[i]);
 		}
 		
-		return temp;
+		//sort the hand
+		sort(begin(hand), end(hand));
+		return hand;
 	}
 	
 };
--- SerialPokerHands/main.cpp
+++ SerialPokerHands/main.cpp
@@ -3,19 +3,29 @@
 #include <iostream>
 using namespace std;
 
-void HandParser(vector<Card> hand){
+bool CheckPair(vector<int> hand){
+	//hand pattern:
+	//XXOOO OXXOO OOXXO OOOXX
 
+	//put the array into a 2d array, making it easier to look at
+	int newHand[5][2];
+	for (int i = 0; i < 5; i++){
+		newHand[i][0] = atoi(to_string(hand[i]).substr(0, to_string(hand[i]).length() - 1).c_str());
+		newHand[i][1] = atoi(to_string(hand[i]).substr(to_string(hand[i]).length() - 1, 1).c_str());
+	}
+	return  newHand[0][0] == newHand[1][0] || newHand[1][0] == newHand[2][0] || newHand[2][0] == newHand[3][0] || newHand[3][0] == newHand[4][0];
 }
 
 int main(){
 
 	Deck cards;
 
-	//cout << cards.deck[1].suit << endl;
-	//for (;;){
-		vector<Card> hand = cards.getHand();
-		
-		HandParser(hand);
-	//}
+	vector<int> hand = cards.getHand();
+
+	bool check = CheckPair(hand);
+
+	//this whole thing could have been done better.
+	
 	return 0;
 }
+
