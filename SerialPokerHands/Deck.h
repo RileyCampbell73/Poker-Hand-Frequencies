8bddb0845a1749a5ec8be4032af583e13db21f94
Riley Campbell <riley.campbell73@gmail.com>
2014-09-29 02:53:39 -0700

    Refactored

--- SerialPokerHands/Deck.h
+++ SerialPokerHands/Deck.h
@@ -2,27 +2,63 @@
 #include <vector>
 #include <algorithm>
 #include <list>
+#include <time.h>
 using namespace std;
 
-//class Card{
-//private:
-//	string value;
-//	//string suit;
-//public:
-//	string suit;
-//	Card(){
-//		value = "";
-//		suit = "";
-//	}
-//	Card(string val, string st){
-//		value = val;
-//		suit = st;
-//	}
-//};
+enum Value{
+	ACE,
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT,
+	NINE,
+	TEN,
+	JACK,
+	QUEEN,
+	KING
+};
+
+enum Suit{
+	SPADES,
+	HEARTS,
+	CLUBS,
+	DIAMONDS
+};
+
+
+
+class Card{
+private:
+	Value value;
+	Suit suit;
+public:
+	Card(){
+		value = ACE;
+		suit = SPADES;
+	}
+	Card(Value val, Suit st){
+		value = val;
+		suit = st;
+	}
+	Value getValue(){
+		return value;
+	}
+	Suit getSuit(){
+		return suit;
+	}
+	friend bool operator <( Card& first,  Card& second)
+	{
+		return first.getValue() < second.getValue();
+	}
+
+};
 
 class Deck{
 private:
-	vector<int> deck;
+	vector<Card> deck;
 public:
 
 	Deck(){
@@ -33,18 +69,30 @@ public:
 		//so 1,1 for example is an ace of spades (1,1)
 		//crude and there definatly a better way to do it. 
 		//but this way will be the easiest in term of sorting and finding matches
-		for (int v = 0; v < 13; v++){
-			for (int s = 0; s < 4; s++){
-				string temp = to_string(values[v]) + to_string(suits[s]);
-				deck.push_back(atoi(temp.c_str()));
+		//for (int v = 0; v < 13; v++){
+		//	for (int s = 0; s < 4; s++){
+		//		string temp = to_string(values[v]) + to_string(suits[s]);
+		//		deck.push_back(atoi(temp.c_str()));
+		//	}
+		//}
+
+		for (int iVal = 0; iVal != 13; iVal++)
+		{
+			for (int iSuit = 0; iSuit != 4; iSuit++)
+			{
+				Card temp((Value)iVal, (Suit)iSuit);
+				deck.push_back(temp);
 			}
 		}
+
+
 
 	}
 
 	//Shuffles the deck and then returns 5 'cards' from the top of the deck
-	vector<int> getHand(){
-		vector<int> hand;
+	vector<Card> getHand(){
+		vector<Card> hand;
+
 		//shuffle the deck
 		random_shuffle(deck.begin(),deck.end());
 		//populate the hand with 5 cards off the deck
@@ -53,7 +101,8 @@ public:
 		}
 		
 		//sort the hand
-		sort(begin(hand), end(hand));
+		sort(hand.begin(), hand.end());
+		//hand.OrderBy(m = > m.Sort).ThenBy(m = > m.Name);
 		return hand;
 	}
 	
--- SerialPokerHands/main.cpp
+++ SerialPokerHands/main.cpp
@@ -8,112 +8,147 @@ using namespace std;
 unsigned int total = 0;
 
 
-bool CheckPair(int hand[5][2]){
+bool CheckPair(vector<Card> hand){
 	//hand pattern:
 	//XXOOO OXXOO OOXXO OOOXX
 
-	return  hand[0][0] == hand[1][0] || hand[1][0] == hand[2][0] || hand[2][0] == hand[3][0] || hand[3][0] == hand[4][0];
+	return  hand[0].getValue() == hand[1].getValue() || hand[1].getValue() == hand[2].getValue() || hand[2].getValue() == hand[3].getValue() || hand[3].getValue() == hand[4].getValue();
 }
-bool CheckTwoPair(int hand[5][2]){
+bool CheckTwoPair(vector<Card> hand){
 	//Hand Pattern
 	//XXII0 0XXII 
 
-	return hand[0][0] == hand[1][0] && hand[2][0] == hand[3][0] || hand[1][0] == hand[2][0] && hand[3][0] == hand[4][0];
+	return hand[0].getValue() == hand[1].getValue() && hand[2].getValue() == hand[3].getValue() || hand[1].getValue() == hand[2].getValue() && hand[3].getValue() == hand[4].getValue();
 }
-bool CheckThreeofKind(int hand[5][2]){
+bool CheckThreeofKind(vector<Card> hand){
 	//Hand Pattern
 	//XXX00 0XXX0 00XXX
 
-	return hand[0][0] == hand[1][0] && hand[0][0] == hand[2][0] || hand[1][0] == hand[2][0] && hand[1][0] == hand[3][0] || hand[2][0] == hand[3][0] && hand[2][0] == hand[4][0];
+	return hand[0].getValue() == hand[1].getValue() && hand[0].getValue() == hand[2].getValue() || hand[1].getValue() == hand[2].getValue() && hand[1].getValue() == hand[3].getValue() || hand[2].getValue() == hand[3].getValue() && hand[2].getValue() == hand[4].getValue();
 
 }
 
-bool CheckFourofKind(int hand[5][2]){
-	//Hand Pattern
-	//XXXX0 0XXXX
-
-	return hand[0][0] == hand[1][0] && hand[0][0] == hand[2][0] && hand[0][0] == hand[3][0] || hand[1][0] == hand[2][0] && hand[1][0] == hand[3][0] && hand[1][0] == hand[4][0];
-
-}
-
-bool CheckFullHouse(int hand[5][2]){
-	//Hand Pattern
-	//XXX00 00XXX
-
-	return hand[0][0] == hand[1][0] && hand[0][0] == hand[2][0] && hand[3][0] == hand[4][0] || hand[2][0] == hand[3][0] && hand[2][0] == hand[4][0] && hand[0][0] == hand[1][0];
-
-}
-
-bool CheckStraight(int hand[5][2]){
+bool CheckStraight(vector<Card> hand){
 	//Hand Pattern
 	//12345 54321
 
-	return hand[0][0] == hand[1][0] + 1 && hand[1][0] == hand[2][0] + 1 && hand[2][0] == hand[3][0] + 1 && hand[3][0] == hand[4][0] + 1 
-		|| hand[0][0] == hand[1][0] - 1 && hand[1][0] == hand[2][0] - 1 && hand[2][0] == hand[3][0] - 1 && hand[3][0] == hand[4][0] - 1;
+	return hand[0].getValue() == hand[1].getValue() + 1 && hand[1].getValue() == hand[2].getValue() + 1 && hand[2].getValue() == hand[3].getValue() + 1 && hand[3].getValue() == hand[4].getValue() + 1
+		|| hand[0].getValue() == hand[1].getValue() - 1 && hand[1].getValue() == hand[2].getValue() - 1 && hand[2].getValue() == hand[3].getValue() - 1 && hand[3].getValue() == hand[4].getValue() - 1;
 
 }
 
-bool CheckStraightFlush(int hand[5][2]){
-	//Hand Pattern
-	//12345 54321
-	//same suit
-
-	return hand[0][0] == hand[1][0] + 1 && hand[1][0] == hand[2][0] + 1 && hand[2][0] == hand[3][0] + 1 && hand[3][0] == hand[4][0] + 1
-		&& hand[0][1] == hand[1][1] && hand[0][1] == hand[2][1] && hand[0][1] == hand[3][1] && hand[0][1] == hand[4][1]
-		|| hand[0][0] == hand[1][0] - 1 && hand[1][0] == hand[2][0] - 1 && hand[2][0] == hand[3][0] - 1 && hand[3][0] == hand[4][0] - 1
-		&& hand[0][1] == hand[1][1] && hand[0][1] == hand[2][1] && hand[0][1] == hand[3][1] && hand[0][1] == hand[4][1];
-
-}
-
-bool CheckRoyalFlush(int hand[5][2]) {
-	// pattern:
-	// XXXXX
-	// A-10-J-Q-K
-	
-	// if the first card isn't an ace and the second card isn't a 10, we don't have a royal flush (ace is 1, 10 is 10)
-	//if (hand[0][0] != 1 || hand[1][0] != 10)
-	//	return false;
-
-	//int suit = hand[0][1];
-	//int temp = 0;
-
-	//// if every card doesn't have the same suit or doesn't increase from 1-13, we don't have a royal flush
-	//for( int i = 0; i < 5; ++i )
-	//{
-	//	if( hand[i][0] <= temp || hand[i][1] != suit )
-	//		return false;
-	//	temp = hand[i][0];
-	//}
-
-
-
-	int suit = hand[0][1];
+bool CheckFlush(vector<Card> hand) {
+	int suit = hand[0].getSuit();
 
 	// if every card doesn't have the same suit we don't have a flush
-	for( int i = 0; i < 5; ++i )
+	for (int i = 0; i < 5; ++i)
 	{
-		if( hand[i][1] != suit )
-			return false;
-	}
-
-	return hand[0][0] == 1 && hand[1][0] == 10 && hand[2][0] == 11 && hand[3][0] == 12 && hand[4][0] == 13;
-}
-
-bool CheckFlush(int hand[5][2]) {
-	int suit = hand[0][1];
-
-	// if every card doesn't have the same suit we don't have a flush
-	for( int i = 0; i < 5; ++i )
-	{
-		if( hand[i][1] != suit )
+		if (hand[i].getSuit() != suit)
 			return false;
 	}
 	return true;
 }
 
-map<string, int> CheckFrequencies()
+bool CheckFullHouse(vector<Card> hand){
+	//Hand Pattern
+	//XXX00 00XXX
+
+	return hand[0].getValue() == hand[1].getValue() && hand[0].getValue() == hand[2].getValue() && hand[3].getValue() == hand[4].getValue()
+		|| hand[2].getValue() == hand[3].getValue() && hand[2].getValue() == hand[4].getValue() && hand[0].getValue() == hand[1].getValue();
+
+}
+
+bool CheckFourofKind(vector<Card> hand){
+	//Hand Pattern
+	//XXXX0 0XXXX
+
+	return hand[0].getValue() == hand[1].getValue() && hand[0].getValue() == hand[2].getValue() && hand[0].getValue() == hand[3].getValue()
+		|| hand[1].getValue() == hand[2].getValue() && hand[1].getValue() == hand[3].getValue() && hand[1].getValue() == hand[4].getValue();
+
+}
+
+bool CheckStraightFlush(vector<Card> hand){
+	//Hand Pattern
+	//12345 54321
+	//same suit
+
+	return hand[0].getValue() == hand[1].getValue() + 1 && hand[1].getValue() == hand[2].getValue() + 1 && hand[2].getValue() == hand[3].getValue() + 1 && hand[3].getValue() == hand[4].getValue() + 1
+		&& hand[0].getSuit() == hand[1].getSuit() && hand[0].getSuit() == hand[2].getSuit() && hand[0].getSuit() == hand[3].getSuit() && hand[0].getSuit() == hand[4].getSuit()
+		|| hand[0].getValue() == hand[1].getValue() - 1 && hand[1].getValue() == hand[2].getValue() - 1 && hand[2].getValue() == hand[3].getValue() - 1 && hand[3].getValue() == hand[4].getValue() - 1
+		&& hand[0].getSuit() == hand[1].getSuit() && hand[0].getSuit() == hand[2].getSuit() && hand[0].getSuit() == hand[3].getSuit() && hand[0].getSuit() == hand[4].getSuit();
+
+}
+
+bool CheckRoyalFlush(vector<Card> hand) {
+	// pattern:
+	// XXXXX
+	// A-10-J-Q-K
+	
+
+	return hand[0].getValue() == ACE && hand[1].getValue() == TEN && hand[2].getValue() == JACK && hand[3].getValue() == QUEEN && hand[4].getValue() == KING
+		&& hand[0].getSuit() == hand[1].getSuit() && hand[0].getSuit() == hand[2].getSuit() && hand[0].getSuit() == hand[3].getSuit() && hand[0].getSuit() == hand[4].getSuit();
+
+}
+
+
+map<string, int> frequencies;
+
+bool CheckFrequencies()
 {
-	map<string,int> frequencies;
+	
+
+
+
+	Deck cards;
+	bool foundAll = false;
+
+		vector<Card> hand = cards.getHand();
+
+
+
+		
+		if(CheckRoyalFlush(hand))
+			frequencies["royalFlush"] += 1;
+		else if(CheckStraightFlush(hand))
+			frequencies["straightFlush"] += 1;
+		else if(CheckFourofKind(hand))
+			frequencies["fourOfAKind"] += 1;
+		else if(CheckFullHouse(hand))
+			frequencies["fullHouse"] += 1;
+		else if(CheckFlush(hand))
+			frequencies["flush"] += 1;
+		else if(CheckStraight(hand))
+			frequencies["straight"] += 1;
+		else if(CheckThreeofKind(hand))
+			frequencies["threeOfAKind"] += 1;
+		else if(CheckTwoPair(hand))
+			frequencies["twoPair"] += 1;
+		else if (CheckPair(hand))
+			frequencies["onePair"] += 1;
+		else
+			frequencies["noPair"] += 1;
+
+
+	//	//int foundCategories = 0;
+	//	//for(it_type iterator = frequencies.begin(); iterator != frequencies.end(); iterator++) {
+	//	//	if( iterator->second != 0 )
+	//	//		foundCategories ++;
+	//	//	if( foundCategories == 9 )
+	//	//		foundAll = true;
+	//	//}
+	//	//if (frequencies["royalFlush"] > 0)
+	//		//foundAll = true;
+	//	
+
+
+	return false;
+}
+
+int main(){
+	Deck cards;
+	vector<Card> hand = cards.getHand();
+	int count = 0;
+
 	frequencies["noPair"] = 0;
 	frequencies["onePair"] = 0;
 	frequencies["twoPair"] = 0;
@@ -125,91 +160,56 @@ map<string, int> CheckFrequencies()
 	frequencies["straightFlush"] = 0;
 	frequencies["royalFlush"] = 0;
 
-
-	Deck cards;
-	bool foundAll = false;
-
-	do
-	{
-		vector<int> hand = cards.getHand();
-		total ++;
-
-		//put the array into a 2d array, making it easier to look at
-		int newHand[5][2];
-		for (int i = 0; i < 5; i++){
-			newHand[i][0] = atoi(to_string(hand[i]).substr(0, to_string(hand[i]).length() - 1).c_str());
-			newHand[i][1] = atoi(to_string(hand[i]).substr(to_string(hand[i]).length() - 1, 1).c_str());
+	do{
+		count++;
+		if (CheckFrequencies()){
+			break;
 		}
-		
-		if(CheckRoyalFlush(newHand))
-			frequencies["royalFlush"] += 1;
-		else if(CheckStraightFlush(newHand))
-			frequencies["straightFlush"] += 1;
-		else if(CheckFourofKind(newHand))
-			frequencies["fourOfAKind"] += 1;
-		else if(CheckFullHouse(newHand))
-			frequencies["fullHouse"] += 1;
-		else if(CheckFlush(newHand))
-			frequencies["flush"] += 1;
-		else if(CheckStraight(newHand))
-			frequencies["straight"] += 1;
-		else if(CheckThreeofKind(newHand))
-			frequencies["threeOfAKind"] += 1;
-		else if(CheckTwoPair(newHand))
-			frequencies["twoPair"] += 1;
-		else if(CheckPair(newHand))
-			frequencies["onePair"] += 1;
-		else
-			frequencies["noPair"] += 1;
-/*
-		typedef std::map<std::string, int>::iterator it_type;
-
-		int foundCategories = 0;
-		for(it_type iterator = frequencies.begin(); iterator != frequencies.end(); iterator++) {
-			if( iterator->second != 0 )
-				foundCategories ++;
-			if( foundCategories == 9 )
-				foundAll = true;
-		}*/
-		if (frequencies["royalFlush"] != 0)
-			foundAll = true;
-		
-	}while (!foundAll);
-
-	return frequencies;
-}
-
-int main(){
-	map<string,int> frequencies = CheckFrequencies();
-
-	//typedef std::map<std::string, int>::iterator it_type;
-	//for(it_type iterator = frequencies.begin(); iterator != frequencies.end(); iterator++) {
-	//	cout << iterator->first << ": " << iterator->second << endl;
+	} while (count <= 275000);
+	////put the array into a 2d array, making it easier to look at
+	//int newHand[5][2];
+	//for (int i = 0; i < 5; i++){
+	//	newHand[i][0] = atoi(to_string(hand[i]).substr(0, to_string(hand[i]).length() - 1).c_str());
+	//	newHand[i][1] = atoi(to_string(hand[i]).substr(to_string(hand[i]).length() - 1, 1).c_str());
 	//}
 
+	//bool check = CheckFlush(newHand);
+
+	/*map<string,int> frequencies = CheckFrequencies();
+
+*/
 
 	cout << setw(64) << right << "Poker Hand Frequency Simulation [SERIAL Version]" << endl;
 	cout << setw(60) << right << "================================================================" << endl;
 	cout << setw(16) << right << "Hand Type" << setw(18) << "Frequency" << setw(30) << "Relative Frequency (%)" << endl;
 	cout << setw(60) << right << "----------------------------------------------------------------" << endl;
-	cout << setw(16) << right << "No Pair" << setw(18) << frequencies["noPair"] << setw(30) << setprecision(6) << (frequencies["noPair"] / (total * 1.0)) * 100 << endl;
-	cout << setw(16) << right << "One Pair" << setw(18) << frequencies["onePair"] << setw(30) << setprecision(6) << (frequencies["onePair"] / (total * 1.0)) * 100 << endl;
-	cout << setw(16) << right << "Two Pair" << setw(18) << frequencies["twoPair"] << setw(30) << setprecision(6) << (frequencies["twoPair"] / (total * 1.0)) * 100 << endl;
-	cout << setw(16) << right << "Three of a Kind" << setw(18) << frequencies["threeOfAKind"] << setw(30) << setprecision(6) << (frequencies["threeOfAKind"] / (total * 1.0)) * 100 << endl;
-	cout << setw(16) << right << "Straight" << setw(18) << frequencies["straight"] << setw(30) << setprecision(6) << (frequencies["straight"] / (total * 1.0)) * 100 << endl;
-	cout << setw(16) << right << "Flush" << setw(18) << frequencies["flush"] << setw(30) << setprecision(6) << (frequencies["flush"] / (total * 1.0)) * 100 << endl;
-	cout << setw(16) << right << "Full House" << setw(18) << frequencies["fullHouse"] << setw(30) << setprecision(6) << (frequencies["fullHouse"] / (total * 1.0)) * 100 << endl;
-	cout << setw(16) << right << "Four of a Kind" << setw(18) << frequencies["fourOfAKind"] << setw(30) << setprecision(6) << (frequencies["fourOfAKind"] / (total * 1.0)) * 100 << endl;
-	cout << setw(16) << right << "Straight Flush" << setw(18) << frequencies["straightFlush"] << setw(30) << setprecision(6) << (frequencies["straightFlush"] / (total * 1.0)) * 100 << endl;
-	cout << setw(16) << right << "Royal Flush" << setw(18) << frequencies["royalFlush"] << setw(30) << setprecision(6) << (frequencies["royalFlush"] / (total * 1.0)) * 100 << endl;
+	cout << setw(16) << right << "No Pair" << setw(18) << frequencies["noPair"] << setw(30) << setprecision(6) << (frequencies["noPair"] / (count * 1.0)) * 100 << endl;
+	cout << setw(16) << right << "One Pair" << setw(18) << frequencies["onePair"] << setw(30) << setprecision(6) << (frequencies["onePair"] / (count * 1.0)) * 100 << endl;
+	cout << setw(16) << right << "Two Pair" << setw(18) << frequencies["twoPair"] << setw(30) << setprecision(6) << (frequencies["twoPair"] / (count * 1.0)) * 100 << endl;
+	cout << setw(16) << right << "Three of a Kind" << setw(18) << frequencies["threeOfAKind"] << setw(30) << setprecision(6) << (frequencies["threeOfAKind"] / (count * 1.0)) * 100 << endl;
+	cout << setw(16) << right << "Straight" << setw(18) << frequencies["straight"] << setw(30) << setprecision(6) << (frequencies["straight"] / (count * 1.0)) * 100 << endl;
+	cout << setw(16) << right << "Flush" << setw(18) << frequencies["flush"] << setw(30) << setprecision(6) << (frequencies["flush"] / (count * 1.0)) * 100 << endl;
+	cout << setw(16) << right << "Full House" << setw(18) << frequencies["fullHouse"] << setw(30) << setprecision(6) << (frequencies["fullHouse"] / (count * 1.0)) * 100 << endl;
+	cout << setw(16) << right << "Four of a Kind" << setw(18) << frequencies["fourOfAKind"] << setw(30) << setprecision(6) << (frequencies["fourOfAKind"] / (count * 1.0)) * 100 << endl;
+	cout << setw(16) << right << "Straight Flush" << setw(18) << frequencies["straightFlush"] << setw(30) << setprecision(6) << (frequencies["straightFlush"] / (count * 1.0)) * 100 << endl;
+	cout << setw(16) << right << "Royal Flush" << setw(18) << frequencies["royalFlush"] << setw(30) << setprecision(6) << (frequencies["royalFlush"] / (count * 1.0)) * 100 << endl;
 	cout << setw(60) << right << "----------------------------------------------------------------" << endl;
-	cout << setw(16) << right << "Hands Generated: " << setw(18) << total << endl;
+	cout << setw(16) << right << "Hands Generated: " << setw(18) << count << endl;
 	cout << setw(60) << right << "----------------------------------------------------------------" << endl;
+
+
+
+
 
 
 	//Deck cards;
 	//bool found = false;
 	//do
+
+	//typedef std::map<std::string, int>::iterator it_type;
+	//for(it_type iterator = frequencies.begin(); iterator != frequencies.end(); iterator++) {
+	//	cout << iterator->first << ": " << iterator->second << endl;
+	//}
 
 	//{
 	//	vector<int> hand = cards.getHand();
@@ -236,6 +236,7 @@ int main(){
 	premade.push_back(131);
 	bool test = CheckRoyalFlush(premade);
 
+<<<<<<< Updated upstream
 	cout << test;*/
 /*
 	Deck cards;
@@ -247,6 +248,20 @@ int main(){
 	} while(!foundRF);
 
 	cout << "found Royal Flush" << endl;*/
+
+	//this whole thing could have been done better.
+
+	//map<string, int> frequencies = CheckFrequencies();
+
+	//typedef std::map<std::string, int>::iterator it_type;
+	//for(it_type iterator = frequencies.begin(); iterator != frequencies.end(); iterator++) {
+		//cout << iterator->first << ": " << iterator->second << endl;
+	//}
+	//vector<int> but = { 11, 101, 111, 121, 131 };
+
+
+	//bool check = CheckRoyalFlush(but);
+
 
 	return 0;
 }
